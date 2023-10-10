6b2a6e404973ec3d27ddc3e796007596931c5d50
Owen Hirschey <ohirs2@uic.edu>
2023-10-10 14:21:02 -0500

    added scripts

---
+++ wip_project/Assets/ToggleDirectionalLight.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+
+public class ToggleDirectionalLight : MonoBehaviour
+{
+    private Light dirLight;
+    public KeyCode toggleKey = KeyCode.Space;  // Key to toggle the light
+
+    private void Start()
+    {
+        dirLight = GetComponent<Light>();
+
+        // Check if the light component is attached
+        if (dirLight == null)
+        {
+            Debug.LogError("No Light component found on this GameObject. Please attach this script to a Directional Light.");
+        }
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(toggleKey))
+        {
+            ToggleLight();
+        }
+    }
+
+    void ToggleLight()
+    {
+        if (dirLight != null)
+        {
+            dirLight.enabled = !dirLight.enabled;
+        }
+    }
+}
